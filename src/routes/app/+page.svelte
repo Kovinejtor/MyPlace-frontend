72d261fa8f5b4610b5b4bf2df61e3e8addf66691
Kovinejtor <95875142+Kovinejtor@users.noreply.github.com>
2024-04-13 12:00:34 +0200

    Add filtering by category on the home page

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import {Input, Card, Button, Label, Select, Textarea} from 'flowbite-svelte';
     import {HomeSolid, CalendarMonthSolid, UsersSolid } from 'flowbite-svelte-icons';
-    import { onMount } from 'svelte';
+    import { onMount} from 'svelte';
     import { writable } from 'svelte/store';
     import { app, storage } from '../../firebase';
     import { getStorage, ref, uploadBytes, getDownloadURL, listAll, getMetadata, deleteObject} from 'firebase/storage';
@@ -32,6 +32,7 @@
 }
 
 let places: Place[] = [];
+let filteredPlaces: Place[] = [];
 
 let selectedPlace: Place | null = null;
 let showImage:boolean = false;
@@ -61,7 +62,9 @@ async function getAllPlacesForCurrentUser() {
             ...place,
             tags: [place.type, place.country, place.city]
         }));
-            console.log('All places:', places);
+            //console.log('All places:', places);
+            filteredPlaces = places;
+            //console.log("Filtered places: ", filteredPlaces);
             retrieveFirstImages();
         } else {
             console.error('Failed to fetch places:', response.statusText);
@@ -186,63 +189,93 @@ function goToNext() {
     currentIndex = (currentIndex + 1) % selectedPlace.images.length;
   }
 }
+
+// @ts-ignore
+function filterPlacesByCategory(category: string): (event: MouseEvent<HTMLDivElement>) => void {  
+  return (event) => {
+        filteredPlaces = places;
+
+        if(category !== "All places"){
+          filteredPlaces = filteredPlaces.filter(place => place.type === category);
+        }
+        console.log(filteredPlaces);
+    };
+}
 </script>
  
 <div id="mainDiv" class="flex justify-center items-center grid grid-flow-row auto-rows-max gap-14">
   <Card class="mt-64 grid grid-cols-4 gap-6 bg-berkeley-blue max-w-6xl text-white shadow-2xl drop-shadow-lg border-2 border-sky-600 text-center">
-    <div class="relative group" >
+    <div on:click={filterPlacesByCategory("Apartment")} role="button" tabindex="0" class="relative group" on:keydown={(event) => {
+      if (event.key === "Enter" || event.key === " ") {
+        filterPlacesByCategory("Apartment");} }}>
       <img src="/apartments.jpg" class="rounded-lg h-48 w-64 hover:outline" alt="product 1"/>
       <p class="absolute top-0 left-0 bg-berkeley-blue text-white px-2 py-1 rounded-tr-lg border-white border-2">Apartments</p>
     </div>
-    <div class="relative group">
+    <div on:click={filterPlacesByCategory("Hotel")} role="button" tabindex="0" class="relative group" on:keydown={(event) => {
+      if (event.key === "Enter" || event.key === " ") {
+        filterPlacesByCategory("Hotel");} }}>
         <img src="/hotels.jpg" class="rounded-lg h-48 w-64 hover:outline" alt="product 1"/>
         <p class="absolute top-0 left-0 bg-berkeley-blue text-white px-2 py-1 rounded-tr-lg border-white border-2">Hotels</p>
     </div>
-    <div class="relative group">
+    <div on:click={filterPlacesByCategory("Hostel")} role="button" tabindex="0" class="relative group" on:keydown={(event) => {
+      if (event.key === "Enter" || event.key === " ") {
+        filterPlacesByCategory("Hostel");} }}>
         <img src="/hostels.jpg" class="rounded-lg h-48 w-64 hover:outline" alt="product 1"/>
         <p class="absolute top-0 left-0 bg-berkeley-blue text-white px-2 py-1 rounded-tr-lg border-white border-2">Hostels</p>
     </div>
-    <div class="relative group">
+    <div on:click={filterPlacesByCategory("Motel")} role="button" tabindex="0" class="relative group" on:keydown={(event) => {
+      if (event.key === "Enter" || event.key === " ") {
+        filterPlacesByCategory("Motel");} }}>
         <img src="/motels.jpeg" class="rounded-lg h-48 w-64 hover:outline" alt="product 1"/>
         <p class="absolute top-0 left-0 bg-berkeley-blue text-white px-2 py-1 rounded-tr-lg border-white border-2">Motels</p>
     </div>
-    <div class="relative group">
+    <div on:click={filterPlacesByCategory("Villa")} role="button" tabindex="0" class="relative group" on:keydown={(event) => {
+      if (event.key === "Enter" || event.key === " ") {
+        filterPlacesByCategory("Villa");} }}>
         <img src="/villas.jpeg" class="rounded-lg h-48 w-64 hover:outline" alt="product 1"/>
         <p class="absolute top-0 left-0 bg-berkeley-blue text-white px-2 py-1 rounded-tr-lg border-white border-2">Villas</p>
     </div>
-    <div class="relative group">
+    <div on:click={filterPlacesByCategory("Cabin")} role="button" tabindex="0" class="relative group" on:keydown={(event) => {
+      if (event.key === "Enter" || event.key === " ") {
+        filterPlacesByCategory("Cabin");} }}>
         <img src="/cabins.jpeg" class="rounded-lg h-48 w-64 hover:outline" alt="product 1"/>
         <p class="absolute top-0 left-0 bg-berkeley-blue text-white px-2 py-1 rounded-tr-lg border-white border-2">Cabins</p>
     </div>
-    <div class="relative group">
-        <img src="/resorts.jpeg" class="rounded-lg h-48 w-64 hover:outline" alt="product 1"/>
-        <p class="absolute top-0 left-0 bg-berkeley-blue text-white px-2 py-1 rounded-tr-lg border-white border-2">Resorts</p>
-    </div>
-    <div class="relative group">
+    <div on:click={filterPlacesByCategory("Cottage")} role="button" tabindex="0" class="relative group" on:keydown={(event) => {
+      if (event.key === "Enter" || event.key === " ") {
+        filterPlacesByCategory("Cottage");} }}>
         <img src="/cottages.jpg" class="rounded-lg h-48 w-64 hover:outline" alt="product 1"/>
         <p class="absolute top-0 left-0 bg-berkeley-blue text-white px-2 py-1 rounded-tr-lg border-white border-2">Cottages</p>
     </div>
+    <div on:click={filterPlacesByCategory("All places")} role="button" tabindex="0" class="relative group" on:keydown={(event) => {
+      if (event.key === "Enter" || event.key === " ") {
+        filterPlacesByCategory("All places");} }}>
+      <img src="/resorts.jpeg" class="rounded-lg h-48 w-64 hover:outline" alt="product 1"/>
+      <p class="absolute top-0 left-0 bg-berkeley-blue text-white px-2 py-1 rounded-tr-lg border-white border-2">All places</p>
+    </div>
   </Card>
 
-  <Card class="grid grid-cols-3 gap-4 bg-berkeley-blue max-w-6xl text-white shadow-2xl drop-shadow-lg border-2 border-sky-600 mt-16 mb-28">
-    {#if $imagesLoaded}
-      {#each places as place (place.id)}
-            <div on:click={() => openDialog(place)} role="button" tabindex="0" on:keydown={(event) => {
-              if (event.key === "Enter" || event.key === " ") {
-                openDialog(place);} }} class="max-w-sm rounded overflow-hidden shadow-lg hover:outline">
-                <img class="w-full h-48" src={place.image} alt="">
-                <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-2">{place.name}</div>
+  {#if filteredPlaces.length !== 0}
+    <Card class="grid grid-cols-3 gap-4 bg-berkeley-blue max-w-6xl text-white shadow-2xl drop-shadow-lg border-2 border-sky-600 mt-16 mb-28">
+      {#if $imagesLoaded}
+        {#each filteredPlaces as place (place.id)}
+              <div on:click={() => openDialog(place)} role="button" tabindex="0" on:keydown={(event) => {
+                if (event.key === "Enter" || event.key === " ") {
+                  openDialog(place);} }} class="max-w-sm rounded overflow-hidden shadow-lg hover:outline">
+                  <img class="w-full h-48" src={place.image} alt="">
+                  <div class="px-6 py-4">
+                      <div class="font-bold text-xl mb-2">{place.name}</div>
+                  </div>
+                  <div class="px-6 pt-4 pb-2">
+                    {#each place.tags as tag}
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{tag}</span>
+                    {/each}
                 </div>
-                <div class="px-6 pt-4 pb-2">
-                  {#each place.tags as tag}
-                      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{tag}</span>
-                  {/each}
               </div>
-            </div>
-          {/each}
-      {/if}
+            {/each}
+        {/if}
   </Card>
+{/if} 
 </div>
 
 
@@ -251,24 +284,25 @@ function goToNext() {
   {#if selectedPlace}
         <div class="absolute inset-0 flex justify-center items-center">
             <Card class="mt-[1300px] md:mt-[1000px] lg:mt-[1000px] grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2 md:gap-4 lg:gap-4 bg-berkeley-blue max-w-2xl text-white shadow-2xl drop-shadow-lg border-2 border-sky-600">
-              <h1 class="text-3xl font-bold text-center md:col-span-2 lg:col-span-2">{selectedPlace.type} {selectedPlace.name}</h1>
+              <h1 class="text-4xl font-bold text-center md:col-span-2 lg:col-span-2">{selectedPlace.type} {selectedPlace.name}</h1>
 
-              <p class="text-center mt-8">Country: {selectedPlace.country}</p>
-              <p class="text-center mt-8">City: {selectedPlace.city}</p>
-              <p class="text-center mt-8">Adress: {selectedPlace.adress}</p>
+              <p class="text-center mt-5">Country: {selectedPlace.country}</p>
+              <p class="text-center mt-5">City: {selectedPlace.city}</p>
+              <p class="text-center mt-5">Adress: {selectedPlace.adress}</p>
               
-              <p class="text-center mt-8">Maximum number of people: {selectedPlace.maxPeople}</p>
-              <p class="text-center mt-8">Number of beds: {selectedPlace.beds}</p>
-              <p class="text-center mt-8">Number of adults: {selectedPlace.adults}</p>
+              <p class="text-center mt-5">Maximum number of people: {selectedPlace.maxPeople}</p>
+              <p class="text-center mt-5">Number of beds: {selectedPlace.beds}</p>
+              <p class="text-center mt-5">Number of adults: {selectedPlace.adults}</p>
               
 
-              <p class="text-center mt-8">Number of children: {selectedPlace.children}</p>
-              <p class="text-center mt-8">Are animals allowed?: {selectedPlace.animals}</p>
-              <p class="text-center mt-8">Is there a parking?: {selectedPlace.parking}</p>
+              <p class="text-center mt-5">Number of children: {selectedPlace.children}</p>
+              <p class="text-center mt-5">Are animals allowed?: {selectedPlace.animals}</p>
+              <p class="text-center mt-5">Is there a parking?: {selectedPlace.parking}</p>
 
-              <p class="text-center mt-8">Minimum nights per reservation: {selectedPlace.minNight}</p>
-              <Textarea class="col-span-2" bind:value={selectedPlace.description}/>
+              <p class="text-center mt-5">Minimum nights per reservation: {selectedPlace.minNight}</p>
+              <p class="col-span-2 text-center mt-5">Description: {selectedPlace.description}</p>
 
+              <p class="text-center col-span-2 text-3xl font-bold mt-12">Images of the place</p>
               {#if showImage}
                 <div class="relative col-span-2">
                   <img src={selectedPlace.images[currentIndex]} class="w-[1200px] h-96" alt="">
